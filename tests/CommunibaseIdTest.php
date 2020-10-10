d1b5c8688d4b864b0abc2a29c30ed46df7c324cb
Misha van Tol <mishavantol@gmail.com>
2020-10-10 22:09:04 +0200

    cover all code

--- tests/CommunibaseIdTest.php
+++ tests/CommunibaseIdTest.php
@@ -16,10 +16,24 @@ class CommunibaseIdTest extends TestCase
     private const VALID_ID_STRING = '5c3e042ea3eeb2010324a8e8';
     private const VALID_ID_STRING_2 = '5c3e042951f0be010443a1d2';
 
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function test_it_can_create(): void
+    {
+        CommunibaseId::create();
+    }
+
     public function test_it_can_be_created_with_an_empty_string(): void
     {
         $id = CommunibaseId::fromString('');
         self::assertTrue($id->isEmpty());
+    }
+
+    public function test_it_casts_null_to_empty_string(): void
+    {
+        $id = CommunibaseId::fromString(null);
+        self::assertSame('', $id->toString());
     }
 
     public function test_it_can_be_created_from_a_valid_string(): void
