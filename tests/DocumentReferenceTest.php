5e2a7f7e16d6a8092a34caa097190c832f64d648
Robin Speekenbrink <robin@kingsquare.nl>
2020-12-01 22:13:28 +0100

    feat(tests): added more tests
    this now also checks for the empty result and the various property references output

--- tests/DocumentReferenceTest.php
+++ tests/DocumentReferenceTest.php
@@ -6,6 +6,9 @@ namespace Communibase\Tests;
 
 use Communibase\CommunibaseId;
 use Communibase\DocumentReference;
+use Communibase\Entity\Address;
+use Communibase\Entity\Email;
+use Communibase\Entity\PhoneNumber;
 use Communibase\Exception\InvalidEntityException;
 use PHPUnit\Framework\TestCase;
 
@@ -15,6 +18,8 @@ use PHPUnit\Framework\TestCase;
  */
 class DocumentReferenceTest extends TestCase
 {
+    private const VALID_ID_STRING = '5c3e042ea3eeb2010324a8e8';
+    private const VALID_ID_STRING_2 = '5c3e042951f0be010443a1d2';
 
     public function test_it_can_be_created(): void
     {
@@ -25,5 +30,114 @@ class DocumentReferenceTest extends TestCase
     {
         $this->expectException(InvalidEntityException::class);
         DocumentReference::create('person', CommunibaseId::create());
+    }
+
+    public function test_it_can_create_reference_for_email(): void
+    {
+        $email = Email::fromEmailAddressData(['_id' => self::VALID_ID_STRING, 'emailAddress' => 'test@kingsquare.nl']);
+        $result = DocumentReference::create('Person', CommunibaseId::fromValidString(self::VALID_ID_STRING_2))->toEmail(
+            $email
+        );
+
+        self::assertSame(
+            [
+                'documentReference' => [
+                    'rootDocumentId' => self::VALID_ID_STRING_2,
+                    'rootDocumentEntityType' => 'Person',
+                ],
+                'path' => [
+                    [
+                        'field' => 'emailAddresses',
+                        'objectId' => self::VALID_ID_STRING,
+                    ],
+                ]
+            ],
+            $result
+        );
+    }
+
+    public function test_it_can_create_reference_for_address(): void
+    {
+        $address = Address::fromAddressData(
+            ['_id' => self::VALID_ID_STRING, 'street' => 'street', 'streetNumber' => 12, 'zipcode' => '1234aa']
+        );
+        $result = DocumentReference::create(
+            'Person',
+            CommunibaseId::fromValidString(self::VALID_ID_STRING_2)
+        )->toAddress(
+            $address
+        );
+
+        self::assertSame(
+            [
+                'documentReference' => [
+                    'rootDocumentId' => self::VALID_ID_STRING_2,
+                    'rootDocumentEntityType' => 'Person',
+                ],
+                'path' => [
+                    [
+                        'field' => 'addresses',
+                        'objectId' => self::VALID_ID_STRING,
+                    ],
+                ]
+            ],
+            $result
+        );
+    }
+
+    public function test_it_can_create_reference_for_phonenumber(): void
+    {
+        $phoneNumber = PhoneNumber::fromPhoneNumberData(
+            ['_id' => self::VALID_ID_STRING, 'areaCode' => 12, 'subscriberNumber' => '1234']
+        );
+        $result = DocumentReference::create(
+            'Person',
+            CommunibaseId::fromValidString(self::VALID_ID_STRING_2)
+        )->toPhoneNumber(
+            $phoneNumber
+        );
+
+        self::assertSame(
+            [
+                'documentReference' => [
+                    'rootDocumentId' => self::VALID_ID_STRING_2,
+                    'rootDocumentEntityType' => 'Person',
+                ],
+                'path' => [
+                    [
+                        'field' => 'phoneNumbers',
+                        'objectId' => self::VALID_ID_STRING,
+                    ],
+                ]
+            ],
+            $result
+        );
+    }
+
+    public function test_it_can_create_reference_for_a_full_entity(): void
+    {
+        $result = DocumentReference::create(
+            'Person',
+            CommunibaseId::fromValidString(self::VALID_ID_STRING_2)
+        )->toEntity();
+
+        self::assertSame(
+            [
+                'documentReference' => [
+                    'rootDocumentId' => self::VALID_ID_STRING_2,
+                    'rootDocumentEntityType' => 'Person',
+                ]
+            ],
+            $result
+        );
+    }
+
+    public function test_it_will_return_empty_if_source_entity_id_is_empty():void
+    {
+        self::assertSame([], DocumentReference::create('Person', CommunibaseId::fromValidString())->toEntity());
+    }
+    public function test_it_will_return_empty_if_source_entity_is_empty():void
+    {
+        self::assertSame([], DocumentReference::create('', CommunibaseId::fromValidString(self::VALID_ID_STRING))->toEntity());
     }
 }
